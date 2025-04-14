56f3eb9dacf84169707cbca0d2f7e7dc924fbad8
PDRBS <pedroo.silva.33@gmail.com>
2025-04-14 19:26:58 -0300

    first commit

---
+++ MathGame/app/src/main/java/com/example/mathgame/QuestionsActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mathgame
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class QuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_questions)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        fun goNextActivity(view:View){
+            val intent = Intent(this, QuestionsActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        val button = findViewById<Button>(R.id.button2)
+
+        button.setOnClickListener { view ->
+            goNextActivity(view)
+        }
+    }
+}
