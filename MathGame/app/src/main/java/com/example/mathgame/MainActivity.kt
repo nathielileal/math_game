4025fc39fd1e17ff48544e4ddb35e544ea0e3b21
PDRBS <pedroo.silva.33@gmail.com>
2025-04-14 20:12:08 -0300

    tela inicial

--- MathGame/app/src/main/java/com/example/mathgame/MainActivity.kt
+++ MathGame/app/src/main/java/com/example/mathgame/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.mathgame
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        fun goNextActivity(view:View){
+            val intent = Intent(this, QuestionsActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        val button = findViewById<Button>(R.id.button2)
+
+        button.setOnClickListener { view ->
+            goNextActivity(view)
         }
     }
 }
--- MathGame/app/src/main/java/com/example/mathgame/QuestionsActivity.kt
+++ MathGame/app/src/main/java/com/example/mathgame/QuestionsActivity.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class QuestionsActivity : AppCompatActivity() {
+
+    private var tries = 0
+    private var right = 0
+    val options = listOf<String>( "+", "-")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,16 +28,48 @@ class QuestionsActivity : AppCompatActivity() {
         }
 
 
-        fun goNextActivity(view:View){
-            val intent = Intent(this, QuestionsActivity::class.java)
+        val n1 : Int = Random.nextInt(0, 99)
+        val n2 : Int = Random.nextInt(0, 99)
+        val bttnAnswer = findViewById<Button>(R.id.buttonAnswer)
+        val bttnNext = findViewById<Button>(R.id.buttonNext)
+        val equacao = findViewById<TextView>(R.id.equacaoView)
 
-            startActivity(intent)
+        equacao.text = "$n1 + $n2"
+        val operator = options.random()
+        fun getOperator(view: View): Int{
+            var calc : Int = 0
+            if(operator == "+"){
+                calc = n1 + n2
+            }else{
+                calc = n1 - n2
+            }
+            return calc
         }
 
-        val button = findViewById<Button>(R.id.button2)
+        fun confirma(view: View){
 
-        button.setOnClickListener { view ->
-            goNextActivity(view)
+            val calc : Int = getOperator(view)
+            val resposta = findViewById<TextView>(R.id.editTextNumber)
+
+            if(calc == resposta.text.toString().toInt()) {
+                right++
+            }
+            tries++
+            resposta.text = ""
         }
+
+        fun next(view: View){
+            val n1 : Int = Random.nextInt(0, 99)
+            val n2 : Int = Random.nextInt(0, 99)
+            val operator = options.random()
+
+            return
+        }
+
     }
 }
+
+
+
+
+
